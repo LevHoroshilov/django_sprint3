872653e7a2524127ee3f36dcbac1d14846dca8fe
Лев Хорошилов <levyahorosh@yandex.ru>
2025-02-26 10:43:32 +0300

    pytest done

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -33,7 +33,7 @@ def category_posts(request, category_slug):
         Q(pub_date__lte=datetime.datetime.now()),
         Q(category__slug=category_slug),
         Q(is_published=True))
-    category = get_object_or_404(Category, is_published=True)
+    category = get_object_or_404(Category.objects.filter(is_published=True), slug=category_slug)
     context = {'category': category,
                'post_list': post_list}
     return render(request, template, context)
